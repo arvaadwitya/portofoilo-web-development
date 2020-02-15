b51bc55f2153d70d9eaf1652ae43f45ff56e8dd0
arvaadwitya <47820177+arvaadwitya@users.noreply.github.com>
2020-02-15 23:28:05 +0700

    Add files via upload

---
+++ TheVanceProject/HomePageScript.js
@@ -0,0 +1,15 @@
+document.getElementById("buttonOnNav").onmouseover = function(){
+    document.getElementById("buttonOnNav").style.borderColor = "white";
+}
+
+document.getElementById("buttonOnNav").onmouseout= function(){
+    document.getElementById("buttonOnNav").style.borderColor = "black";
+}
+
+document.getElementById("buttonInsideJumbotron").onmouseover = function(){
+    document.getElementById("buttonInsideJumbotron").style.color = "black";
+}
+
+document.getElementById("buttonInsideJumbotron").onmouseout= function(){
+    document.getElementById("buttonInsideJumbotron").style.color = "white";
+}
